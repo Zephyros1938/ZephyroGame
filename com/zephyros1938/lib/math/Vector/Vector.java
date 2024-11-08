4597bb8650b7074b1af2fc446490de641b0cc0d0
Zephyros1938.org <xtrau505true@gmail.com>
2024-11-08 21:27:25 +0000

    V0.03.07-alpha

--- com/zephyros1938/lib/math/Vector/Vector.java
+++ com/zephyros1938/lib/math/Vector/Vector.java
@@ -13,6 +13,35 @@ public class Vector {
         public Vector2 add(Vector2 other) {
             return new Vector2(this.X + other.X, this.Y + other.Y);
         }
+
+        public Vector2 subtract(Vector2 other) {
+            return new Vector2(this.X - other.X, this.Y - other.Y);
+        }
+
+        public Vector2 multiply(Vector2 other) {
+            return new Vector2(this.X * other.X, this.Y * other.Y);
+        }
+
+        public Vector2 dot(double scalar) {
+            return new Vector2(this.X * scalar, this.Y * scalar);
+        }
+
+        public double magnitude() {
+            return Math.sqrt(this.X * this.X + this.Y * this.Y);
+        }
+
+        public Vector2 normalize() {
+            double mag = magnitude();
+            if (mag == 0) {
+                return new Vector2(0, 0);
+            }
+            return new Vector2(this.X / mag, this.Y / mag);
+        }
+
+        @Override
+        public String toString() {
+            return "(" + this.X + "," + this.Y + ")";
+        }
     }
 
     public class Vector3 {
@@ -28,6 +57,35 @@ public class Vector {
 
         public Vector3 add(Vector3 other) {
             return new Vector3(this.X + other.X, this.Y + other.Y, this.Z + other.Z);
+        }
+
+        public Vector3 subtract(Vector3 other) {
+            return new Vector3(this.X - other.X, this.Y - other.Y, this.Z - other.Z);
+        }
+
+        public Vector3 multiply(Vector3 other) {
+            return new Vector3(this.X * other.X, this.Y * other.Y, this.Z * other.Z);
+        }
+
+        public Vector3 dot(double scalar) {
+            return new Vector3(this.X * scalar, this.Y * scalar, this.Z * scalar);
+        }
+
+        public double magnitude() {
+            return Math.sqrt(this.X * this.X + this.Y * this.Y + this.Z * this.Z);
+        }
+
+        public Vector3 normalize() {
+            double mag = magnitude();
+            if (mag == 0) {
+                return new Vector3(0, 0, 0);
+            }
+            return new Vector3(this.X / mag, this.Y / mag, this.Z / mag);
+        }
+
+        @Override
+        public String toString() {
+            return "(" + this.X + "," + this.Y + "," + this.Z + ")";
         }
     }
 
@@ -46,6 +104,30 @@ public class Vector {
 
         public Vector4 add(Vector4 other) {
             return new Vector4(this.X + other.X, this.Y + other.Y, this.Z + other.Z, this.W + other.W);
+        }
+
+        public Vector4 subtract(Vector4 other) {
+            return new Vector4(this.X - other.X, this.Y - other.Y, this.Z - other.Z, this.W - other.W);
+        }
+
+        public Vector4 multiply(Vector4 other) {
+            return new Vector4(this.X * other.X, this.Y * other.Y, this.Z * other.Z, this.W * other.W);
+        }
+
+        public Vector4 dot(double scalar) {
+            return new Vector4(this.X * scalar, this.Y * scalar, this.Z * scalar, this.W * scalar);
+        }
+
+        public double magnitude() {
+            return Math.sqrt(this.X * this.X + this.Y * this.Y + this.Z * this.Z + this.W * this.W);
+        }
+
+        public Vector4 normalize() {
+            double mag = magnitude();
+            if (mag == 0) {
+                return new Vector4(0, 0, 0, 0);
+            }
+            return new Vector4(this.X / mag, this.Y / mag, this.Z / mag, this.W / mag);
         }
     }
 }
