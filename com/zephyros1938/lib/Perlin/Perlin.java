d485fffaad62417e5eb6583bfabda70284d5aa4d
Zephyros1938.org <xtrau505true@gmail.com>
2024-11-08 21:28:39 +0000

    V0.03.07A-alpha

--- com/zephyros1938/lib/Perlin/Perlin.java
+++ com/zephyros1938/lib/Perlin/Perlin.java
@@ -1,5 +1,10 @@
 package com.zephyros1938.lib.Perlin;
 
+/*
+ * RESOURCES:
+ * - https://jaysmito101.hashnode.dev/perlins-noise-algorithm
+ */
+
 public final class Perlin { // TODO: add perlin functions
 
 }
