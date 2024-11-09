f668c3f62783b9f2996b536df58535664087e685
Xtrau505 <xtrau505true@gmail.com>
2024-11-08 19:40:51 -0600

    V0.03.08-alpha

--- com/zephyros1938/lib/Perlin/Perlin.java
+++ com/zephyros1938/lib/Perlin/Perlin.java
@@ -8,21 +8,14 @@ import com.zephyros1938.lib.math.Vector.Vector.Vector2;
  */
 
 public class Perlin { // TODO: add perlin functions
+    class _2d {
+        private static final int B = 256;
+        private Vector2[] gradients = new Vector2[B + B + 2];
+        int[] permutation = new int[B + B + 2];
 
-    private static int B = 256;
-    private static Vector2 gradients;
+        public Vector2 noise(Vector2 in) {
 
-    public Vector2 noise(Vector2 in) {
-        Vector2 P0 = new Vector2(Math.floor(in.X), Math.floor(in.Y));
-        Vector2 P1 = new Vector2(Math.floor(in.X), Math.floor(in.Y) + 1.0);
-        Vector2 P2 = new Vector2(Math.floor(in.X) + 1.0, Math.floor(in.Y));
-        Vector2 P3 = new Vector2(Math.floor(in.X) + 1.0, Math.floor(in.Y) + 1.0);
-
-        Vector2 UV_P0 = P0.randomUnitVector();
-        Vector2 UV_P1 = P1.randomUnitVector();
-        Vector2 UV_P2 = P2.randomUnitVector();
-        Vector2 UV_P3 = P3.randomUnitVector();
-
-        return in;
+            return in;
+        }
     }
 }
--- com/zephyros1938/lib/math/Vector/Vector.java
+++ com/zephyros1938/lib/math/Vector/Vector.java
@@ -2,40 +2,56 @@ package com.zephyros1938.lib.math.Vector;
 
 public class Vector {
     public static class Vector2 {
-        public double X;
-        public double Y;
+        public double x;
+        public double y;
 
-        public Vector2(double X, double Y) {
-            this.X = X;
-            this.Y = Y;
+        /* CONSTRUCTORS */
+
+        public Vector2(double x, double y) {
+            this.x = x;
+            this.y = y;
         }
 
+        public static Vector2[] fromArray(double[] values) {
+            if (values.length % 2 != 0) {
+                throw new IllegalArgumentException("Array length must be even to create Vector2 pairs.");
+            }
+
+            Vector2[] vectors = new Vector2[values.length / 2];
+            for (int i = 0; i < values.length; i += 2) {
+                vectors[i / 2] = new Vector2(values[i], values[i + 1]);
+            }
+            return vectors;
+        }
+
+        /* METHODS */
+
         public Vector2 add(Vector2 other) {
-            return new Vector2(this.X + other.X, this.Y + other.Y);
+            return new Vector2(this.x + other.x, this.y + other.y);
         }
 
         public Vector2 subtract(Vector2 other) {
-            return new Vector2(this.X - other.X, this.Y - other.Y);
+            return new Vector2(this.x - other.x, this.y - other.y);
         }
 
         public Vector2 multiply(Vector2 other) {
-            return new Vector2(this.X * other.X, this.Y * other.Y);
+            return new Vector2(this.x * other.x, this.y * other.y);
         }
 
         public Vector2 dot(double scalar) {
-            return new Vector2(this.X * scalar, this.Y * scalar);
+            return new Vector2(this.x * scalar, this.y * scalar);
         }
 
         public double magnitude() {
-            return Math.sqrt(this.X * this.X + this.Y * this.Y);
+            return Math.sqrt(this.x * this.x + this.y * this.y);
         }
 
-        public Vector2 normalize() {
+        public Vector2 normal() {
             double mag = magnitude();
             if (mag == 0) {
                 return new Vector2(0, 0);
             }
-            return new Vector2(this.X / mag, this.Y / mag);
+            return new Vector2(this.x / mag, this.y / mag);
         }
 
         public Vector2 randomUnitVector() {
@@ -44,45 +60,61 @@ public class Vector {
             double uv_y = Math.cos(theta);
 
             Vector2 uv = new Vector2(uv_x, uv_y);
-            
+
             return this.add(uv);
         }
 
         @Override
         public String toString() {
-            return "(" + this.X + "," + this.Y + ")";
+            return "(" + this.x + "," + this.y + ")";
         }
     }
 
     public static class Vector3 {
-        public double X;
-        public double Y;
-        public double Z;
+        public double x;
+        public double y;
+        public double z;
 
-        public Vector3(double X, double Y, double Z) {
-            this.X = X;
-            this.Y = Y;
-            this.Z = Z;
+        /* CONSTRUCTORS */
+
+        public Vector3(double x, double y, double z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
         };
 
+        public static Vector3[] fromArray(double[] values) {
+            if (values.length % 3 != 0) {
+                throw new IllegalArgumentException("Array length must be a multiple of 3 to create Vector3 pairs.");
+            }
+
+            Vector3[] vectors = new Vector3[values.length / 3];
+            for (int i = 0; i < values.length; i += 3) {
+                vectors[i / 3] = new Vector3(values[i], values[i + 1], values[i + 2]);
+            }
+            return vectors;
+        }
+
+        /* METHODS */
+
         public Vector3 add(Vector3 other) {
-            return new Vector3(this.X + other.X, this.Y + other.Y, this.Z + other.Z);
+            return new Vector3(this.x + other.x, this.y + other.y, this.z + other.z);
         }
 
         public Vector3 subtract(Vector3 other) {
-            return new Vector3(this.X - other.X, this.Y - other.Y, this.Z - other.Z);
+            return new Vector3(this.x - other.x, this.y - other.y, this.z - other.z);
         }
 
         public Vector3 multiply(Vector3 other) {
-            return new Vector3(this.X * other.X, this.Y * other.Y, this.Z * other.Z);
+            return new Vector3(this.x * other.x, this.y * other.y, this.z * other.z);
         }
 
         public Vector3 dot(double scalar) {
-            return new Vector3(this.X * scalar, this.Y * scalar, this.Z * scalar);
+            return new Vector3(this.x * scalar, this.y * scalar, this.z * scalar);
         }
 
         public double magnitude() {
-            return Math.sqrt(this.X * this.X + this.Y * this.Y + this.Z * this.Z);
+            return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
         }
 
         public Vector3 normalize() {
@@ -90,46 +122,62 @@ public class Vector {
             if (mag == 0) {
                 return new Vector3(0, 0, 0);
             }
-            return new Vector3(this.X / mag, this.Y / mag, this.Z / mag);
+            return new Vector3(this.x / mag, this.y / mag, this.z / mag);
         }
 
         @Override
         public String toString() {
-            return "(" + this.X + "," + this.Y + "," + this.Z + ")";
+            return "(" + this.x + "," + this.y + "," + this.z + ")";
         }
     }
 
     public static class Vector4 {
-        public double X;
-        public double Y;
-        public double Z;
-        public double W;
+        public double x;
+        public double y;
+        public double z;
+        public double w;
 
-        Vector4(double X, double Y, double Z, double W) {
-            this.X = X;
-            this.Y = Y;
-            this.Z = Z;
-            this.W = W;
+        /* CONSTRUCTORS */
+
+        Vector4(double x, double y, double z, double w) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
         };
 
+        public static Vector4[] fromArray(double[] values) {
+            if (values.length % 4 != 0) {
+                throw new IllegalArgumentException("Array length must be a multiple of 4 to create Vector4 pairs.");
+            }
+
+            Vector4[] vectors = new Vector4[values.length / 4];
+            for (int i = 0; i < values.length; i += 4) {
+                vectors[i / 4] = new Vector4(values[i], values[i + 1], values[i + 2], values[i + 3]);
+            }
+            return vectors;
+        }
+
+        /* METHODS */
+
         public Vector4 add(Vector4 other) {
-            return new Vector4(this.X + other.X, this.Y + other.Y, this.Z + other.Z, this.W + other.W);
+            return new Vector4(this.x + other.x, this.y + other.y, this.z + other.z, this.w + other.w);
         }
 
         public Vector4 subtract(Vector4 other) {
-            return new Vector4(this.X - other.X, this.Y - other.Y, this.Z - other.Z, this.W - other.W);
+            return new Vector4(this.x - other.x, this.y - other.y, this.z - other.z, this.w - other.w);
         }
 
         public Vector4 multiply(Vector4 other) {
-            return new Vector4(this.X * other.X, this.Y * other.Y, this.Z * other.Z, this.W * other.W);
+            return new Vector4(this.x * other.x, this.y * other.y, this.z * other.z, this.w * other.w);
         }
 
         public Vector4 dot(double scalar) {
-            return new Vector4(this.X * scalar, this.Y * scalar, this.Z * scalar, this.W * scalar);
+            return new Vector4(this.x * scalar, this.y * scalar, this.z * scalar, this.w * scalar);
         }
 
         public double magnitude() {
-            return Math.sqrt(this.X * this.X + this.Y * this.Y + this.Z * this.Z + this.W * this.W);
+            return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z + this.w * this.w);
         }
 
         public Vector4 normalize() {
@@ -137,7 +185,12 @@ public class Vector {
             if (mag == 0) {
                 return new Vector4(0, 0, 0, 0);
             }
-            return new Vector4(this.X / mag, this.Y / mag, this.Z / mag, this.W / mag);
+            return new Vector4(this.x / mag, this.y / mag, this.z / mag, this.w / mag);
+        }
+
+        @Override
+        public String toString() {
+            return "(" + this.x + "," + this.y + "," + this.z + "," + this.w + ")";
         }
     }
 }
